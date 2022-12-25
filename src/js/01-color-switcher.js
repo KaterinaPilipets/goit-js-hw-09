8837c3f73d787a8bf9eb59951336659278777284
KaterinaPilipets <ket.pil.glass@gmail.com>
2022-12-26 01:04:40 +0200

    done task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const startBtn = body.querySelector('[data-start]');
+const stopBtn = body.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', changeColorStart);
+stopBtn.addEventListener('click', changeColorStop);
+stopBtn.setAttribute('disabled', '');
+
+function changeColorStop() {
+  clearInterval(timerId);
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', '');
+}
+
+function changeColorStart() {
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+    startBtn.setAttribute('disabled', '');
+    stopBtn.removeAttribute('disabled');
+  }, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
